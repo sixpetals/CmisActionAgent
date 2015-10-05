6fe8b744b731e70ec657d1ac289295485db6d0f3
git -v <takuma.sugimoto@aegif.jp>
2015-10-05 16:19:05 +0900

    Add first project files

---
+++ CmisActionAgentSolution/CmisActionAgentConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using jp.aegif.cmis.tools;
+
+namespace CmisActionAgentConsole
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            var conn = new CmisConnector();
+
+
+        }
+    }
+}
